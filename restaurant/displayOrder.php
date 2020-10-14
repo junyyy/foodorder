d90662006e0a7cadfca7392c371ddef9ace86575
Jun <junyangthebest@gmail.com>
2020-10-14 23:09:30 +1100

    create display order page

--- restaurant/displayOrder.php
+++ restaurant/displayOrder.php
@@ -1,1 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf8">
+    <title>Display Orders</title>
+    <link rel="stylesheet" type="text/css" href="display.css">
+</head>
+
+<body>
+    <div class="topnav">
+        <a href="#">Home</a>
+        <a href="#">PlaceHolder</a>
+        <a href="#">PlaceHolder</a>
+    </div>
+</body>
+
+</html>

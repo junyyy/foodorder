bd5d7c58e0768bb8f9f48f89ecb2d09be69f0ac0
Jun <junyangthebest@gmail.com>
2020-10-20 22:01:26 +1100

    create display order page

--- restaurant/displayOrder.php
+++ restaurant/displayOrder.php
@@ -9,9 +9,27 @@
 
 <body>
     <div class="topnav">
-        <a href="#">Home</a>
-        <a href="#">PlaceHolder</a>
-        <a href="#">PlaceHolder</a>
+        <a hre="" target="_top">Home</a>
+    </div>
+    <hr>
+    <?php
+    include_once 'mysqlConnect.php';
+//    $sql = 'select * from order';
+//    $stmt = $dbh->prepare($sql);
+//    echo var_dump($stmt);
+//    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+//        echo $row;
+//        print $row['order_id'] . "\n";
+//        print "here";
+//    }
+
+    ?>
+    <div class="orderBlock">
+        <p>
+            id: abc <br>
+            table: <br>
+            items:
+        </p>
     </div>
 </body>
 
--- restaurant/mysqlConnect.php
+++ restaurant/mysqlConnect.php
@@ -5,6 +5,17 @@ $user = USER;
 $password = PASSWORD;
 try {
     $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = 'select * from `food` where 1';
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        print $row['name'];
+        echo "<br>";
+
+    }
 } catch (PDOException $e) {
     echo 'Connection failed ' . $e->getMessage();
 }
